069d38c0286bc6c9cf0fc285e230846c65aac449
Emma J <codemzy@gmail.com>
2017-03-07 16:55:58 +0000

    Added server.js file

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use('/html', express.static(process.cwd() + '/public/html'));
+app.use('/js', express.static(process.cwd() + '/public/js'));
+app.use('/css', express.static(process.cwd() + '/public/css'));
+app.use('/img', express.static(process.cwd() + '/public/img'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// set the port
+app.set('port', (process.env.PORT || 8080));
+
+// routes
+// const routes = require('./routes/routes.js');
+// routes(app);
+
+// start the server
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port', app.get('port'));
+});
