ffe6d9ea82090c5a8077ebb0484a8da10bf8bac4
Emma J <codemzy@gmail.com>
2017-03-08 09:06:05 +0000

    Installed ejs-locals and wired up

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const engine = require('ejs-locals');
 
 app.use('/html', express.static(process.cwd() + '/public/html'));
 app.use('/js', express.static(process.cwd() + '/public/js'));
 app.use('/css', express.static(process.cwd() + '/public/css'));
 app.use('/img', express.static(process.cwd() + '/public/img'));
 
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+app.set('views', __dirname + '/views');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
